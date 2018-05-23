c489c523e21933ce34d757ec22e59db197b9c917
Alessio Faieta <alessio.faieta97@gmail.com>
2018-05-23 09:30:21 +0200

    Add syscalls wrappers

---
+++ src/syscalls_wrappers.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <sys/ipc.h>
+
+#include "syscalls_wrappers.h"
+
+void log(const char *format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+}
+
+void program_fail(const char *format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	log(format, ap);
+	va_end(ap);
+	exit(1);
+}
+
+
+void syscall_fail(const char *message) {
+	perror(message);
+	exit(1);
+}
+
+
+int my_open(const char *pathname, int flags) {
+	int fd = open(pathname, flags);
+	if (fd < 1) {
+		syscall_fail(pathname);
+	}
+	return fd;
+}
+/*
+int my_open(const char *pathname, int flags, mode_t mode) {
+	int fd = open(pathname, flags, mode);
+	if (fd < 1) {
+		syscall_fail(pathname);
+	}
+	return fd;
+}
+*/
+
+char *my_read(int fd) {
+	const int MAX_BUF_SIZE = 10;
+	char *str = (char*) malloc(MAX_BUF_SIZE * sizeof(char));	// TODO: sostituire con wrapper nostro? (restituisce NULL in caso di errore)
+	if (read(fd, str, MAX_BUF_SIZE) < 1) {
+		syscall_fail("Impossibile leggere il PID del daemon");
+	}
+	return str;
+}
+
+
+
+/* ***** MESSAGE QUEUES ***** */
+
+key_t my_ftok(const char *pathname, int proj_id) {
+	key_t message_queue_key = ftok(pathname, proj_id);
+	if (message_queue_key < 0) {
+		syscall_fail("Errore durante la generazione della chiave per la message queue");
+	}
+	return message_queue_key;
+}
+
+int my_msgget(key_t key, int msgflg) {
+	int message_queue_id = msgget(key, msgflg);
+	if (message_queue_id < 0) {
+		syscall_fail("Errore durante la creazione della message queue");
+	}
+	return message_queue_id;
+}
+
+int my_msgsnd(int msqid, const void *msgp, size_t msgsz, int msgflg) {
+	int ret_val = msgsnd(msqid, msgp, msgsz, msgflg);
+	if (ret_val < 0) {
+		syscall_fail("Errore durante l'invio del messaggio");
+	}
+	return ret_val;
+}
+
+ssize_t my_msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg) {
+	int ret_val = msgrcv(msqid, msgp, msgsz, msgtyp, msgflg);
+	if (ret_val < 0) {
+		syscall_fail("Errore durante la ricezione del messaggio");
+	}
+	return ret_val;
+}
+
+int my_msgctl(int msqid, int cmd, struct msqid_ds *buf) {
+	int ret_val = msgctl(msqid, cmd, buf);
+	if (ret_val < 0) {
+		syscall_fail("Errore durante l'eliminazione della message queue");
+	}
+	return ret_val;
+}
+
+
+
+
---
+++ src/syscalls_wrappers.h
@@ -0,0 +1,19 @@
+#ifndef SYSCALLS_WRAPPERS_H
+#define SYSCALLS_WRAPPERS_H
+
+
+#include <sys/types.h>
+#include <sys/msg.h>
+
+void syscall_fail(const char *message);
+int my_open(const char *pathname, int flags);
+char *my_read(int fd);
+
+/* ***** MESSAGE QUEUES ***** */
+key_t my_ftok(const char *pathname, int proj_id);
+int my_msgget(key_t key, int msgflg);
+int my_msgsnd(int msqid, const void *msgp, size_t msgsz, int msgflg);
+ssize_t my_msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg);
+int my_msgctl(int msqid, int cmd, struct msqid_ds *buf);
+
+#endif
